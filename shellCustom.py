4628e7863a524cebd19a9dd79575384755213929
Conor Cronin <conorcronin7@hotmail.com>
2017-10-25 05:18:14 +0100

    Add files via upload

--- shellCustom.py
+++ shellCustom.py
@@ -7,24 +7,22 @@ Create an executable shell that locks down user functionality
 pwd and ifconfig should be executed as external commands
 '''
 
-import getpass, time, datetime, os, pwdfile, ipconfig
+import getpass, sys, time, datetime, os, pwdfile, ipconfig
 from cmd import Cmd
 
 
 class CustomShell(Cmd):
 
-    def welcomescreen(self):
+    def do_whoami(self, args):
         username = getpass.getuser()
         print("Welcome Conor's Custom Shell: %s\n" % username)
-        print("If you would like some help please type 'helpme'.\n")
-        print('Please enter a command to begin:')
 
-    def do__dt(self):
+    def do_dt(self, args):
         i = datetime.datetime.now()
         print('The date and time are as follows (YYMMDDHHMMSS): %s%s%s%s%s%s\n' %
               (i.year, i.day, i.month, i.hour, i.minute, i.second))
 
-    def do_ud(self):
+    def do_ud(self, args):
         dir = os.getenv("HOME")
         dirinfo = os.stat(dir)
         inode_id = dirinfo.st_ino
@@ -40,27 +38,29 @@ class CustomShell(Cmd):
             config = ipconfig.main(args)
             print('The information for %s is:' % args, config)
 
-    def do_pw(self):
+    def do_pw(self, args):
         cwd = pwdfile.main()
         print('Your current working directory is: %s\n' % cwd)
 
-    def do_whereami(self):
+    def do_whereami(self, args):
         print("********************************")
         print("                                ")
         print("You are in Conor's Custom Shell.")
         print("                                ")
         print("********************************")
 
-    def do_exit(self):
+    def do_exit(self, args):
+        pos = 'y'
+        neg = 'n'
         answer = input('Are you sure you want to exit? (y/n)\n')
-        if answer == 'y':
+        if answer is pos:
             print('Thank you for using the custom shell, good bye!')
             time.sleep(3)
-            os._exit(1)
-        elif answer == 'n':
-            CustomShell.welcomescreen(self)
+            sys.exit(1)
+        elif answer is neg:
+            print('Carry on so!')
 
-    def do_helpme(self):
+    def do_helpme(self, args):
         print('Hello there, it seems you need some help!\n'
               'Please choose from one of the following commands:\n')
         print(' ___________________________________________________')
@@ -74,8 +74,11 @@ class CustomShell(Cmd):
         print('|                    User can specify interface also|')
         print('|pw          -       Displays information of current|')
         print('|                    working directory              |')
+        print('|whoami      -       Show current users username    |')
+        print('|exit        -       Quits the shell                |')
         print('|___________________________________________________|')
 
 
-main = CustomShell()
-main.cmdloop()
+sys = CustomShell()
+sys.prompt = '=> '
+sys.cmdloop("Please enter a command, for help type 'helpme':")
